d16ddd942f299ea7f4f5618773402e68d11ab42e
berkezap <ihsanberke.ozsap@std.yeditepe.edu.tr>
2023-10-04 12:23:41 +0300

    Update App.jsx

--- useTransition/src/App.jsx
+++ useTransition/src/App.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react";
+import { useState, useTransition } from "react";
 
 function App() {
   const [input, setInput] = useState("");
   const [myList, setMyList] = useState([]);
+  const [isPending, startTransition] = useTransition();
 
   const handleChange = (e) => {
     setInput(e.target.value);
-    const myArray = [];
-    for (let i = 0; i < 2000; i++) {
-      myArray.push(e.target.value);
-    }
-    setMyList(myArray);
+
+    startTransition(() => {
+      const myArray = [];
+      for (let i = 0; i < 2000; i++) {
+        myArray.push(e.target.value);
+      }
+      setMyList(myArray);
+    });
   };
 
   return (
     <>
       <input type="text" value={input} onChange={handleChange} />
-      {myList.map((item, index) => {
-        return <div key={index}>{item}</div>;
-      })}
+
+      {isPending
+        ? "Yükleniyor"
+        : myList.map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
     </>
   );
 }
